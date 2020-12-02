e6e5f4e66edf015512db6817f7d73d4a8f29b156
Llion Marc Evans <llion.evans@swansea.ac.uk>
2020-12-02 12:25:24 +0000

    Bugfix

--- pl_curve.py
+++ pl_curve.py
@@ -63,7 +63,7 @@ def calculate_gini(data):
         return math.nan
 
     # Mean absolute difference
-    mad = np.abs(np.subtract.outer(data.DataArray, data.DataArray)).mean()
+    mad = np.abs(np.subtract.outer(data.to_numpy(), data.to_numpy())).mean()
     # Relative mean absolute difference
     rmad = mad / np.mean(data)
     # Gini coefficient
